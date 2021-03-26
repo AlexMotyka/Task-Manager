eb910ee500d12ff4353d7307a0c36347476ad765
Alex Motyka <motykaalex@gmail.com>
2021-03-26 15:26:43 -0400

    Add avatar test. Refactor beforeEach()

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,26 +1,9 @@
 const request = require('supertest')
-const jwt = require('jsonwebtoken')
-const mongoose = require('mongoose')
-const app = require('../src/app')
 const User = require('../src/models/user')
-const { response } = require('../src/app')
+const app = require('../src/app')
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
 
-const userOneId = new mongoose.Types.ObjectId()
-
-const userOne = {
-    _id: userOneId,
-    name: "Mike",
-    password: "Pitofha133123!",
-    email: "mike@asodjoadsl.com",
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.SECRET)
-    }]
-}
-
-beforeEach(async () => {
-    await User.deleteMany()
-    await new User(userOne).save()
-})
+beforeEach(setupDatabase)
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
@@ -79,4 +62,34 @@ test('Should not delete profile for unauthenticated user', async () => {
         .delete('/users/me')
         .send()
         .expect(401)
+})
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({ name: "Billy Bob" })
+        .expect(200)
+    
+    const user = await User.findById(userOneId)
+    expect(user.name).toEqual("billy bob")
+})
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({ zodiac: "Taurus" })
+        .expect(400)
 })
